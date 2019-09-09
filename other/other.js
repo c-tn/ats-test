e77aea5385fcb2271f4ff140a75b19a0f944b047
Vlad K <Uladzislau.Katykhin@itechart-group.com>
2019-09-09 15:08:07 +0300

    [ADD] added city gen

--- other/other.js
+++ other/other.js
@@ -1,52 +1,157 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-canvas.width = 1600;
-canvas.height = 1200;
+canvas.width = 2048;
+canvas.height = 1080;
 
 ctx.fillStyle = '#000';
 ctx.fillStyle = '#fff';
 ctx.strokeStyle = '#fff';
 
-let lines = [
-    {
-        x1: 300, y1: 300,
-        x2: 500, y2: 500
-    }
-]
+let offset = {
+    x: 0,
+    y: 0,
+    offsetX: 0,
+    offsetY: 0,
+    isClick: false
+}
+
+let lines = [];
+ctx.font = "25px sans-serif";
 
 function drawLines() {
-    ctx.fillStyle = '#fff';
+    ctx.fillStyle = 'rgba(255, 255, 255, .3)';
     ctx.strokeStyle = '#fff';
 
     lines.forEach(line => {
-        ctx.save();
-            ctx.beginPath();
+        // if (line.inter) {
+        //     ctx.fillRect(
+        //         line.inter.x - offset.x - 5,
+        //         line.inter.y - offset.y - 5,
+        //         10,
+        //         10
+        //     );
+        // }
 
-            ctx.moveTo(line.x1, line.y1);
-            ctx.lineTo(line.x2, line.y2);
+        ctx.beginPath();
 
-            ctx.stroke();
+        ctx.moveTo(line.x1 - offset.x, line.y1 - offset.y);
+        ctx.lineTo(line.x2 - offset.x, line.y2 - offset.y);
 
-            lines.forEach(line2 => {
-                if (line === line2) return;
+        ctx.stroke();
 
-                const point = checkIntersects(line, line2);
+        if (line.build) {
+            ctx.save();
+                ctx.translate(
+                    line.x1 - offset.x,
+                    line.y1 - offset.y,
+                );
+    
+                ctx.rotate(line.angle);
+    
+                ctx.fillRect(0, 0, 10, 10);
+            ctx.restore();
+        }
+    });
+}
 
-                if (
-                    point.x >= Math.min(line.x1, line.x2) &&
-                    point.x <= Math.max(line.x1, line.x2) &&
-                    point.x >= Math.min(line2.x1, line2.x2) &&
-                    point.x <= Math.max(line2.x1, line2.x2) &&
-                    point.y >= Math.min(line.y1, line.y2) &&
-                    point.y <= Math.max(line.y1, line.y2) &&
-                    point.y >= Math.min(line2.y1, line2.y2) &&
-                    point.y <= Math.max(line2.y1, line2.y2)                    
-                ) {
-                    ctx.fillRect(point.x - 5, point.y - 5, 10, 10);
-                }
-            });
-        ctx.restore();
+function loop() {
+    if (offset.isClick) {
+        offset.x -= window.innerWidth / 50 - offset.offsetX / 25;
+        offset.y -= window.innerHeight / 50 - offset.offsetY / 25;
+    }
+
+    ctx.fillStyle = '#000';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    drawLines(lines);
+
+    requestAnimationFrame(loop);
+}
+
+const angleOffset = 25;
+let angle = (angleOffset * Math.random() * Math.PI / 180) - (angleOffset * 2 * Math.random() * Math.PI / 180);
+
+const maxAngle = 360 * Math.PI / 180;
+let length = 25;
+
+function generateRoads(lvl = 0, angle, lastLine, count = 20, currentCount = 0, isNew) {
+    if (!lastLine) {
+        lastLine = {
+            x1: 100,
+            y1: canvas.height / 2,
+            x2: 100 + Math.cos(angle) * length,
+            y2: canvas.height / 2 + Math.sin(angle) * length
+        }
+
+        lines.push(lastLine);
+    }
+
+    angle += (angleOffset * Math.random() * Math.PI / 180) - (angleOffset * Math.random() * Math.PI / 180);
+
+    if (angle > 0 + maxAngle) {
+        angle = maxAngle;
+    }
+    if (angle < 0 - maxAngle) {
+        angle = 0 - maxAngle;
+    }
+
+    let newLine = {
+        x1: lastLine.x2,
+        y1: lastLine.y2,
+        x2: lastLine.x2 + Math.cos(angle) * length,
+        y2: lastLine.y2 + Math.sin(angle) * length,
+        angle
+    };
+
+    check(newLine);
+
+    newLine.build = Math.random() > 0.7 && !newLine.inter;
+
+    if (!isNew && !newLine.inter && Math.random() > 0.8 && lvl < 2) {
+        let direction = Math.random() > 0.5
+            ? 1
+            : -1;
+
+        generateRoads(lvl + 1, angle + 90 * Math.PI / 180 * direction, lastLine, count, 5, true);
+    }
+
+    lines.push(newLine);
+
+    if (lvl <= 3 && currentCount < count && !newLine.inter) {
+        generateRoads(lvl, angle, newLine, count, currentCount + 1);
+    }
+}
+
+generateRoads(0, angle);
+
+function check(line) {
+    lines.forEach(line2 => {
+        if (
+            line === line2 ||
+            line.x2 === line2.x1 ||
+            line.y2 === line2.y1 ||
+            line.x1 === line2.x2 ||
+            line.y1 === line2.y2
+        ) return;
+
+        const point = checkIntersects(line, line2);
+
+        if (
+            point.x >= Math.min(line.x1, line.x2) &&
+            point.x <= Math.max(line.x1, line.x2) &&
+            point.x >= Math.min(line2.x1, line2.x2) &&
+            point.x <= Math.max(line2.x1, line2.x2) &&
+            point.y >= Math.min(line.y1, line.y2) &&
+            point.y <= Math.max(line.y1, line.y2) &&
+            point.y >= Math.min(line2.y1, line2.y2) &&
+            point.y <= Math.max(line2.y1, line2.y2)                    
+        ) {
+            line.x2 = point.x;
+            line.y2 = point.y;
+            line.inter = point;
+        }
     });
 }
 
@@ -61,44 +166,35 @@ function checkIntersects(l1, l2) {
     const y3 = l2.y1;
     const y4 = l2.y2;
 
-    const d = (x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2);
-    const nx = (x1*y2 - x2*y1) * (x3 - x4) - (x3*y4 - x4*y3) * (x1 - x2);
-    const ny = (x1*y2 - x2*y1) * (y3 - y4) - (x3*y4 - x4*y3) * (y1 - y2);
+    let m12 = (y2 - y1) / (x2 - x1);
+    let m34 = (y4 - y3) / (x4 - x3);
+    let m = m34 / m12;
+    let x = (x1 - y1 / m12 - m * x3 + y3 / m12) / (1 - m);
+    let y = m12 * (x - x1) + y1;
 
-    return {
-        x: Math.round(nx / d),
-        y: Math.round(ny / d)
-    }
+    return { x, y }
 }
 
-function loop() {
-    ctx.fillStyle = '#000';
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    drawLines();
+canvas.addEventListener('mousedown', ({ offsetX, offsetY }) => {
+    offset.isClick = true;
 
-    requestAnimationFrame(loop);
-}
-
-canvas.addEventListener('mousemove', ({ offsetX, offsetY }) => {
-    const x = offsetX / window.innerWidth * canvas.width;
-    const y = offsetY / window.innerHeight * canvas.height;
-
-    lines[lines.length - 1].x2 = x;
-    lines[lines.length - 1].y2 = y;
+    offset.offsetX = offsetX;
+    offset.offsetY = offsetY;
 });
 
-canvas.addEventListener('click', ({ offsetX, offsetY }) => {
-    const x = offsetX / window.innerWidth * canvas.width;
-    const y = offsetY / window.innerHeight * canvas.height;
+canvas.addEventListener('mouseup', () => {
+    offset.isClick = false;
+});
 
-    lines.push({
-        x1: x,
-        y1: y,
-        x2: x,
-        y2: y
-    });
+canvas.addEventListener('mousemove', ({ offsetX, offsetY }) => {
+    if (!offset.isClick) return;
+
+    offset.offsetX = offsetX;
+    offset.offsetY = offsetY;
+});
+
+canvas.addEventListener('wheel', () => {
+    generateRoads(0, angle, lines[lines.length - 1]);
 });
 
 loop();
---
+++ other/shipGen.js
@@ -0,0 +1,138 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.style.width = '200px';
+canvas.style.height = '200px';
+
+canvas.width = canvas.height = 200;
+
+let sprite;
+let seed = '';
+let hue = 0;
+let saturation = 0;
+let data = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 1, 1,
+    0, 2, 2, 0, 0, 0, 1, 1, 1,
+    0, 0, 1, 0, 0, 0, 1, 1, 1,
+    0, 0, 1, 0, 0, 1, 1, 1, 1,
+    0, 0, 1, 0, 1, 1, 1, 1, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+let seedInput = document.getElementById('seed');
+
+seedInput.addEventListener('input', ({ target }) => {
+    seed = target.value;
+
+    draw();
+});
+
+function generateSprite({
+    hue = 0.5,
+    saturation = 0.5,
+    width = 9,
+    height = 16,
+    data,
+    seed
+} = {}) {
+    let generatedMask = new Mask({
+        data,
+        width,
+        height,
+        isMirrorX: true
+    });
+
+    let sprite = new Sprite(generatedMask, {
+        hue,
+        saturation,
+        seed,
+        isColored: true
+    });
+
+    let resizedSprite = sprite.resize(sprite.canvas, 5);
+
+    return resizedSprite;
+}
+
+function createSprite() {
+    sprite = generateSprite({ hue, seed, saturation, data });
+}
+
+let cells = [];
+
+createCells();
+
+function createCells() {
+    let wrapper = document.getElementById('mask');
+
+    for (let i = 0; i < 144; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+
+        if (data[i]) {
+            cell.classList.add('filled');
+        }
+
+        wrapper.appendChild(cell);
+        cells.push(cell);
+    }
+
+    wrapper.addEventListener('click', ({ target }) => {
+        if (!target.classList.contains('cell')) return;
+    
+        let i = cells.findIndex(d => d === target);
+    
+        data[i] = +!data[i];
+
+        cells[i].classList.toggle('filled');
+
+        draw();
+    });
+}
+
+function draw() {
+    ctx.clearRect(0, 0, 200, 200);
+
+    createSprite();
+
+    ctx.drawImage(
+        sprite,
+        canvas.width / 2 - sprite.width / 2,
+        canvas.height / 2 - sprite.height / 2
+    );
+}
+
+let hueWrapper = document.getElementById('hue');;
+hueWrapper.addEventListener('input', ({ target }) => {
+    hue = +target.value;
+
+    draw();
+})
+
+let saturationWrapper = document.getElementById('saturation');
+saturationWrapper.addEventListener('input', ({ target }) => {
+    saturation = +target.value;
+
+    draw();
+})
+
+let resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', () => {
+    data = data.map(v => v = 0);
+
+    cells.forEach(cell => cell.classList.remove('filled'));
+
+    draw();
+})
+
+draw();
